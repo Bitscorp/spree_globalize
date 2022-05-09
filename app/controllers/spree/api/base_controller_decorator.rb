d54663a1787b189e8c5d7f7d4303f79f75f695c5
Alexandr Korsak <alex.korsak@gmail.com>
2022-05-09 13:14:43 +0200

    create module for autoloading

--- app/controllers/spree/api/base_controller_decorator.rb
+++ app/controllers/spree/api/base_controller_decorator.rb
@@ -1,7 +1,15 @@
-spree_version = Gem::Version.new(::Spree::VERSION)
+module Spree
+  module Api
+    module BaseControllerDecorator
 
-if spree_version > Gem::Version.new("4.x")
-  require_relative 'v2/base_controller_decorator'
-else
-  require_relative 'v1/base_controller_decorator'
+      spree_version = Gem::Version.new(::Spree::VERSION)
+
+      if spree_version > Gem::Version.new("4.x")
+        require_relative 'v2/base_controller_decorator'
+      else
+        require_relative 'v1/base_controller_decorator'
+      end
+
+    end
+  end
 end
